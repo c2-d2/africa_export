827d00ee0d10ce9f68be306783896c5fdeebfaa1
Rene <rene.niehus@gmx.de>
2020-05-15 11:20:35 +0200

    update

--- code/numbers_text.R
+++ code/numbers_text.R
@@ -4,7 +4,9 @@ mt %>% mutate( fvolume_od = ifelse( is.na(fvolume_od), 0 , fvolume_od ) ) ->mt
 # look at variable
 mt %>% glimpse()
 
-## global Ratio Wuhan/non-Wuhan
+
+
+# global Ratio Wuhan/non-Wuhan --------------------------------------------
 mt %>% 
                 filter(is_global_d==1) %>% 
                 mutate( force_imp=prevalence_o*fvolume_od ) %>% 
@@ -15,8 +17,8 @@ mt %>%
                 pivot_wider(names_from = is_wuhan, values_from = sum_force_imp) %>% 
                 mutate( R=`0`/`1`)
 
-mt %>% filter( destination_country=="United States" ) %>% 
-                group_by(is_wuhan,scenario) %>% 
-                summarise( n=n(),
-                           sum_prev=mean(prevalence_o) )
-# Scenario 4 has 1 mio times less prevalence than 
+
+
+# total number of predicted cases for Africa ------------------------------
+
+
