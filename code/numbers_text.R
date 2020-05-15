be65eb51744bd872016b8ea0eea3080627a375d8
Rene <rene.niehus@gmx.de>
2020-05-15 10:43:17 +0200

    script for numbers in the text

---
+++ code/numbers_text.R
@@ -0,0 +1,16 @@
+# compute numbers for text
+mt <- read_csv("./data/master_table.csv",guess_max = Inf)
+mt %>% mutate( fvolume_od = ifelse( is.na(fvolume_od), 0 , fvolume_od ) ) ->mt
+
+mt %>% glimpse()
+
+## global Ratio Wuhan/non-Wuhan
+mt %>% 
+                filter(is_global_d==1) %>% 
+                mutate( force_imp=prevalence_o*fvolume_od ) %>% 
+                #
+                group_by(is_wuhan,scenario) %>% 
+                summarise( sum_force_imp=sum(force_imp) ) %>% ungroup() %>% 
+                #
+                pivot_wider(names_from = is_wuhan, values_from = sum_force_imp) %>% 
+                mutate( R=`0`/`1`)
