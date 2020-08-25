c340d1b05d66e140bebe7f195057cb6757619e36
goshgondar2018 <31212044+goshgondar2018@users.noreply.github.com>
2020-08-25 10:13:18 -0700

    Update new_ARR.R

--- code/new_ARR.R
+++ code/new_ARR.R
@@ -1,5 +1,5 @@
 # read in cumulative cases in Hubei and in all other provinces, as of Feb 29
-data_feb29=read.csv("table_getAreaStat_en_2020-02-29.csv")
+data_feb29=read.csv("./data/table_getAreaStat_en_2020-02-29.csv")
 confirmed_cases_nh<-data_feb29%>%
   group_by(provinceShortName)%>%
   filter(provinceShortName!="Hubei")%>%
@@ -12,7 +12,7 @@ confirmed_cases_h<-data_feb29%>%
   summarise(cumulative_cases_Hubei=sum(confirmedCount))
 
 # read in cumulative deaths in Hubei and in all other provinces, as of Mar 19 (to account for 19 day delay from symptom onset to death)
-data_mar19=read.csv("table_getAreaStat_en_2020-03-19.csv")
+data_mar19=read.csv("./data/table_getAreaStat_en_2020-03-19.csv")
 confirmed_deaths_nh<-data_mar19%>%
   group_by(provinceShortName)%>%
   filter(provinceShortName!="Hubei")%>%
@@ -25,7 +25,7 @@ confirmed_deaths_h<-data_mar19%>%
   summarise(cumulative_deaths_Hubei=sum(deadCount))
 
 # assumpe of equal infection infection fatality rates in provinces outside of Hubei and provinces within Hubei
-IFR_nh=IFR_h=0.66
+IFR_nh=IFR_h=0.66 # Verity et al. estimate
 
 # Extract total infections in Hubei and in all other provinces
 total_infections_h=confirmed_deaths_h$cumulative_deaths_Hubei/IFR_h
