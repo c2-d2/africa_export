15e23f4d7fc6e2cff59bb6a5e44c3637899290db
goshgondar2018 <31212044+goshgondar2018@users.noreply.github.com>
2020-08-26 19:23:27 -0700

    Update new_ARR.R

--- code/new_ARR.R
+++ code/new_ARR.R
@@ -12,14 +12,14 @@ confirmed_cases_h<-data_feb29%>%
   summarise(cumulative_cases_Hubei=sum(confirmedCount))
 
 # read in cumulative deaths in Hubei and in all other provinces, as of Mar 19 (to account for 19 day delay from symptom onset to death)
-data_mar19=read.csv("./data/table_getAreaStat_en_2020-03-19.csv")
-confirmed_deaths_nh<-data_mar19%>%
+data_mar13=read.csv("./data/table_getAreaStat_en_2020-03-13.csv")
+confirmed_deaths_nh<-data_mar13%>%
   group_by(provinceShortName)%>%
   filter(provinceShortName!="Hubei")%>%
   summarise(cumulative_deaths_province=sum(deadCount))%>%
   summarise(cumulative_deaths_total=sum(cumulative_deaths_province))
 
-confirmed_deaths_h<-data_mar19%>%
+confirmed_deaths_h<-data_mar13%>%
   group_by(provinceShortName)%>%
   filter(provinceShortName=="Hubei")%>%
   summarise(cumulative_deaths_Hubei=sum(deadCount))
@@ -31,8 +31,11 @@ IFR_nh=IFR_h=0.66 # Verity et al. estimate
 total_infections_h=confirmed_deaths_h$cumulative_deaths_Hubei/IFR_h
 total_infections_nh=confirmed_deaths_nh$cumulative_deaths_total/IFR_nh
 
+# define proportion symptomatic - to yield number of total infections who are symptomatic
+prop_symp= 0.22 # https://www.bmj.com/content/369/bmj.m1375 
+
 # Compute the ascertainment rate ratio using total_infections as defined above
-ARR=(confirmed_cases_nh$cumulative_cases_total/total_infections_nh)/(confirmed_cases_h$cumulative_cases_Hubei/total_infections_h)
+ARR=(confirmed_cases_nh$cumulative_cases_total/(total_infections_nh*prop_symp))/(confirmed_cases_h$cumulative_cases_Hubei/(total_infections_h*prop_symp))
 
 ####### CHECK: Direct computation - b/c IFRs cancel out
 ARR=(confirmed_cases_nh$cumulative_cases_total/confirmed_deaths_nh$cumulative_deaths_total)/
